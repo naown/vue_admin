fee2dffc0e9a53e9ad30b1b7fce26de6464ec88c
chenjian <739535718@qq.com>
2021-09-30 15:55:01 +0800

    测试分支

---
+++ src/main/java/com/naown/utils/Test.java
@@ -0,0 +1,16 @@
+package com.naown.utils;
+
+import com.naown.mapper.UserRoleMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @DATE: 2021/9/15 4:26 下午
+ * @AUTHOR: chenjian
+ */
+@Component
+public class Test {
+    @Autowired
+    private UserRoleMapper userRoleMapper;
+
+}
--- src/test/java/com/naown/AdminApplicationTests.java
+++ src/test/java/com/naown/AdminApplicationTests.java
@@ -3,11 +3,21 @@ package com.naown;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+
 @SpringBootTest
 class AdminApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    public static void main(String[] args) {
+        String[] data = {"123","456","789","987"};
+        String s = String.join(",", data);
+        System.out.println(s);
+        String[] split = s.split(",");
+        System.out.println(Arrays.toString(split));
     }
 
 }
